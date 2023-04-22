c12e35f4dafb85e333da5e61b0b3b6f40995eeea
SofiMetel <60064845+SofiMetel@users.noreply.github.com>
2023-04-22 12:19:48 +0100

    attempt2-reacting-to-feedback
    1.bank.py solving floating point problem
    2. read.me file update

--- README.md
+++ README.md
@@ -19,6 +19,49 @@ Enter amount2(in cent): 180
 The sum of these is €2.45
 ```
 
+
+In feedback lecturer asked not to use floating point numbers. They always will appear if we do division like this (sum/100)
+Main idea behind it is the fact that floating point numbers can create more errors. 
+https://www.geeksforgeeks.org/floating-point-error-in-python/ 
+They recommended "Check floating point section in python documentation for more such behaviours."
+
+I read tutorial about floating point and found out that decimal package can help reduce rounding error changing number from float to decimal
+https://docs.python.org/3/tutorial/floatingpoint.html#tut-fp-issues
+
+```
+decimalnum=Decimal.from_float((sum/100))
+print(type(decimalnum))
+print(output.format(decimalnum))
+```
+
+```
+$ python bank.py
+Enter amount1(in cent): 77
+Enter amount1(in cent): 190
+<class 'float'>
+The sum of these is €2.67
+<class 'decimal.Decimal'>
+The sum of these is €2.67
+```
+But Later I started to hesitate because it looks like the number becomes at first float after \100 and only then turns into decimal and from python docs it wasn`t clear for me if in this case floating-point-error would be a problems for us or not
+So I decided to turn integer into decimal and then perform division
+```
+sum = Decimal(input1) + Decimal(input2) # step 2.
+print(type(sum))
+print(type(sum/100))
+print(output.format(sum/100)) #step 3
+```
+
+
+```
+Enter amount1(in cent): 65
+Enter amount1(in cent): 180
+<class 'decimal.Decimal'>
+<class 'decimal.Decimal'>
+The sum of these is €2.45
+```
+
+
 # week 03 
 accounts.py
 
--- bank.py
+++ bank.py
@@ -6,11 +6,13 @@
 #Enter amount1(in cent): 65
 #Enter amount2(in cent): 180
 #The sum of these is €2.45
+from decimal import Decimal
+
 
 input1 = input("Enter amount1(in cent): ") #step 1.
 input2 = input("Enter amount1(in cent): ") 
 
-sum = int(input1) + int(input2) # step 2.
+#sum = int(input1) + int(input2) # step 2.
 
 
 # to achive step 3 I use example from https://www.w3schools.com/python/python_string_formatting.asp
@@ -20,9 +22,40 @@ sum = int(input1) + int(input2) # step 2.
 
 
 output = "The sum of these is €{:.2f}" 
-print(output.format(sum/100))
+
+#this piece of code was created to check data type
+#a=sum/100
+#print(type(a))
+#print(output.format(sum/100))
 
 
 
-#or one more way for step 3.
-#print(f"The sum of these is €{sum/100}")
+'''
+In feedback lecturer asked not to use floating point numbers. They always will appear if we do division like this (sum/100)
+Main idea behind it is the fact that floating point numbers can create more errors. 
+https://www.geeksforgeeks.org/floating-point-error-in-python/ 
+They recommended "Check floating point section in python documentation for more such behaviours."
+
+I read tutorial about floating point and found out that decimal package can help reduce rounding error changing number from float to decimal
+https://docs.python.org/3/tutorial/floatingpoint.html#tut-fp-issues
+
+'''
+
+#this piece of code was created to check data type
+#decimalnum=Decimal.from_float((sum/100))
+#print(type(decimalnum))
+#print(output.format(decimalnum))
+
+#print(output.format(Decimal.from_float((sum/100))))
+
+'''
+But Later I started to hesitate because it looks like the number becomes at first float
+after /100 and only then turns into decimal and from python docs it wasn`t clear for 
+me if in this case floating-point-error would be a problems for us or not.
+So I decided to turn integer into decimal and then perform division
+'''
+
+sum = Decimal(input1) + Decimal(input2) # step 2.
+print(type(sum))
+print(type(sum/100))
+print(output.format(sum/100)) #step 3
